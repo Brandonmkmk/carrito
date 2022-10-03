70420977dfe9bba8d524ffcec77dc1b4fb3e5e1f
Brandonmkmk <brandonxd611@gmail.com>
2022-10-03 11:07:35 -0500

    recuperando datos de objetos seleccionados

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,15 +3,29 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
+
 const agregarCurso = (e) => {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log('presionaste el boton de carrito');
+        const curso = e.target.parentElement;
+        const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('p.precio').textContent,
+        id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+        cantidad : 1
+        }
+        agregarCarrito(infoCurso);
 
-    }else{
-        console.log('tas loco pa');
     }
+
 }
+    const agregarCarrito = curso => {
+        listadoCarrito = [...listadoCarrito, curso]
+        console.log(listadoCarrito);
+    }
+
 
 const cargarEventListener = () => {
     //Agregar funcion de carga de cursos al carrito
